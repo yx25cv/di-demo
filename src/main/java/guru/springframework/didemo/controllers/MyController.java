b05245627b63af318ea8e34474ebc7bd78754543
yx25cv <42892130+yx25cv@users.noreply.github.com>
2018-12-10 11:38:13 +0100

    Pokazano kako radi @primary anotacija. Closes #4

--- src/main/java/guru/springframework/didemo/controllers/MyController.java
+++ src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -1,12 +1,19 @@
 package guru.springframework.didemo.controllers;
 
+import guru.springframework.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello!!");
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -1,7 +1,8 @@
 package guru.springframework.didemo.controllers;
 
-import guru.springframework.didemo.services.GreetingServiceImpl;
+import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,14 +10,18 @@ public class PropertyInjectedController {
 
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingServiceImpl;
+
+    //ako hoces kroz proerty da injectujes neki bean kroz interfejs mozes to da uradis ovako, injectujes interface i onda u property name odaberes ime beana, i spring boot ce to
+    //znati da parsira ili naravno kroz queifier
 
 //    public GreetingServiceImpl getGreetingService () {
 //        return greetingService;
 //    }
 
     public String sayHello(){
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 
 
