60f03deed368f92e5b23dad5a5fdbd8def753858
yx25cv <42892130+yx25cv@users.noreply.github.com>
2018-12-10 11:05:47 +0100

    Razdvojena implemetacija GreetingService interjejsa na posebne servise za setter i konstruktor. Closes #3

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.didemo.controllers;
 
 
 import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -11,9 +12,12 @@ public class ConstructorInjectedController {
     private GreetingService greetingService;
 
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
+    //ovde dodajes ovaj qualifier zato sto ima tri beana koji implemetiraju interface GreetingService, i bukvalno onda ne zna koji bean da injectuje
+    //zato mu kazes naziv beana koji treba da se injectuje, a rekli smo da je konvencija da nazive bean-a spring boot dodeljuje kao naziv klase samo stavi
+    //prvlo slovo malo.
 
     public String sayHello(){
         return greetingService.sayGreeting();
--- src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,7 +17,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
